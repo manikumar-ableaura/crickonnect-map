5ae60bc8d7733b792bf878f10fa8def66aeab636
Mani kumar <manikumar@ableaura.com>
2025-01-15 16:20:58 +0530

    Updates

---
+++ precache-manifest.d480316b88cbc084d953507fa0189cb3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b82564c02e8b61897f957661690825d",
+    "url": "/crickonnect.map/index.html"
+  },
+  {
+    "revision": "b0ba1a31567f0d8c3d55",
+    "url": "/crickonnect.map/static/css/main.72e783ab.chunk.css"
+  },
+  {
+    "revision": "c9cd3f714d097eb93fa4",
+    "url": "/crickonnect.map/static/js/2.52d6f074.chunk.js"
+  },
+  {
+    "revision": "b0ba1a31567f0d8c3d55",
+    "url": "/crickonnect.map/static/js/main.69262930.chunk.js"
+  },
+  {
+    "revision": "43aee21c03118e5f1096",
+    "url": "/crickonnect.map/static/js/runtime~main.76824470.js"
+  },
+  {
+    "revision": "75b29478422fe01ed321d2afb8db3c1b",
+    "url": "/crickonnect.map/static/media/R1.75b29478.jpg"
+  },
+  {
+    "revision": "43fee1c2a9b733e50cfe79b267933a33",
+    "url": "/crickonnect.map/static/media/R2.43fee1c2.jpg"
+  },
+  {
+    "revision": "50786bc6be0a5429254e0a1409d61a66",
+    "url": "/crickonnect.map/static/media/R3.50786bc6.jpg"
+  },
+  {
+    "revision": "db93c381dbc004b216d38253d34951a1",
+    "url": "/crickonnect.map/static/media/a.db93c381.jpg"
+  },
+  {
+    "revision": "e4771ee653f196741046c420143c18a0",
+    "url": "/crickonnect.map/static/media/crik.e4771ee6.png"
+  },
+  {
+    "revision": "841d219777545c8980f584101f3cdce5",
+    "url": "/crickonnect.map/static/media/g3.841d2197.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crickonnect/precache-manifest.6734d7900d861e43b56e43408a2d3020.js"
+  "/crickonnect.map/precache-manifest.d480316b88cbc084d953507fa0189cb3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crickonnect/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crickonnect.map/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
