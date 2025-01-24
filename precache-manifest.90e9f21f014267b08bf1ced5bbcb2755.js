7a4f9f1e1739b4f3f0631d5e571e3a99e96224f3
Mani kumar <manikumar@ableaura.com>
2025-01-24 19:47:06 +0530

    Updates

---
+++ precache-manifest.90e9f21f014267b08bf1ced5bbcb2755.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a2f319e28d60b584e27e32a5f33284",
+    "url": "/crickonnect-map/index.html"
+  },
+  {
+    "revision": "d46da0e88d47638975d7",
+    "url": "/crickonnect-map/static/css/main.72b6470f.chunk.css"
+  },
+  {
+    "revision": "d04ff45025a045632fe8",
+    "url": "/crickonnect-map/static/js/2.790af9b1.chunk.js"
+  },
+  {
+    "revision": "d46da0e88d47638975d7",
+    "url": "/crickonnect-map/static/js/main.915f8b42.chunk.js"
+  },
+  {
+    "revision": "74abb85dec0e4b4a55c7",
+    "url": "/crickonnect-map/static/js/runtime~main.bf21a1df.js"
+  },
+  {
+    "revision": "75b29478422fe01ed321d2afb8db3c1b",
+    "url": "/crickonnect-map/static/media/R1.75b29478.jpg"
+  },
+  {
+    "revision": "43fee1c2a9b733e50cfe79b267933a33",
+    "url": "/crickonnect-map/static/media/R2.43fee1c2.jpg"
+  },
+  {
+    "revision": "50786bc6be0a5429254e0a1409d61a66",
+    "url": "/crickonnect-map/static/media/R3.50786bc6.jpg"
+  },
+  {
+    "revision": "db93c381dbc004b216d38253d34951a1",
+    "url": "/crickonnect-map/static/media/a.db93c381.jpg"
+  },
+  {
+    "revision": "e4771ee653f196741046c420143c18a0",
+    "url": "/crickonnect-map/static/media/crik.e4771ee6.png"
+  },
+  {
+    "revision": "841d219777545c8980f584101f3cdce5",
+    "url": "/crickonnect-map/static/media/g3.841d2197.jpg"
+  }
+]);
